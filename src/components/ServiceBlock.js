72e9fbface95f672f01930d544cef356bc350abe
Alisha Garric <AlishaGarric@Alishas-iMac.local>
2019-01-18 12:59:13 -0800

    services page responsive

--- src/components/ServiceBlock.js
+++ src/components/ServiceBlock.js
@@ -7,22 +7,41 @@ import { ColumnWrapper } from '../components/Column'
 import Planting from '../images/planting.jpg'
 import { Wrapper } from '../components/block'
 
+const Header = styled(H3)`
+    margin-bottom: 30px;
+    @media (max-width: 2000px) {
+        margin-bottom: 1.5vw;
+    }
+    @media (max-width: 600px ) {
+        margin-bottom: 10px;
+    }
+`
+
 const Image = styled(Wrapper)`
-    height: 25vw;
+    height: 500px;
+    @media (max-width: 2000px) {
+        height: 25vw;
+    }
     min-height: 300px;
 `
 
 const Column = styled(ColumnWrapper)`
-    margin: 8vw .5vw 0 .5vw;
+  margin: 160px 10px 0px 10px;
+  @media (max-width: 2000px) {
+    margin: 8vw 0.5vw 0 0.5vw;
+  }
+  @media (max-width: 600px) {
+    margin: 15vw .5vw 0 .5vw;
+  }
 `
 
 
 const ServiceBlock = props => (  
         <Column>
-            <H3 weight="700">
+            <Header weight="700">
                 {props.header}
-            </H3>
-            <Image bgImage={Planting} />
+            </Header>
+            <Image padding="0" bgImage={Planting} />
             <PLarge>
                 {props.text}
             </PLarge>
--- src/pages/services.js
+++ src/pages/services.js
@@ -6,12 +6,26 @@ import Summary from '../components/Summary'
 import Container from '../components/Container'
 import SEO from '../components/SEO'
 import { Wrapper, HeaderBlock, Block } from '../components/Block'
-import TwoColRow from '../components/TwoColRow'
+import { TwoColRowWrapper } from '../components/TwoColRow'
 import ServiceBlock from '../components/ServiceBlock'
 import { H2, H3, P, PLarge, HeaderText } from '../components/Headings'
 import FirewoodBlock from '../components/FirewoodBlock'
 
+const TwoColRow = styled(TwoColRowWrapper)`
+    @media (max-width: 700px){
+        flex-direction: column;
+    }
+`
 
+const ServiceList = styled(Wrapper)`
+  padding: 0 100px 200px 100px;
+  @media (max-width: 2000px) {
+    padding: 0 5vw 10vw 5vw;
+  }
+  @media (max-width: 500px) {
+    padding: 0 0 10vw 0;
+  }
+`
 
 const Home = ({ data }) => {
     const postNode = {
@@ -33,17 +47,19 @@ const Home = ({ data }) => {
                 Tree Care Services
                 </HeaderText>
             </HeaderBlock>
-            <TwoColRow>
-                <ServiceBlock header="Removal" text="We pride ourselves on our ability to extricate such very large and dangerous trees while doing no damage to the fine surroundings." bText="Learn More" dest="/" />
-                <ServiceBlock header="Planting" text="One tree or a hundred trees, no job is too big or small. There are few things better than watching a properly planted tree grow and thrive …" bText="Learn More" dest="/" />
-            </TwoColRow>
-            <TwoColRow>
-                <ServiceBlock header="Trimming" text="As a premier local tree service, we strive to offer the highest quality pruning or trimming possible. All our pruning is done to ISA…" bText="Learn More" dest="/" />
-                <ServiceBlock header="Surgery" text="Supplemental structural support for trees and branches. We’re experts in the cabling and bracing procedures that are the current industry…" bText="Learn More" dest="/" />
-            </TwoColRow>
-            <TwoColRow>
-                <ServiceBlock header="Plant Health Care (PHC)" text="Our team will do everything possible to improve and maintain a tree’s health. We’ll do what is right for every tree." bText="Learn More" dest="/" />
-            </TwoColRow>
+            <ServiceList bgColor="#FFFFFF">
+                <TwoColRow>
+                    <ServiceBlock header="Removal" text="We pride ourselves on our ability to extricate such very large and dangerous trees while doing no damage to the fine surroundings." bText="Learn More" dest="/" />
+                    <ServiceBlock header="Planting" text="One tree or a hundred trees, no job is too big or small. There are few things better than watching a properly planted tree grow and thrive …" bText="Learn More" dest="/" />
+                </TwoColRow>
+                <TwoColRow>
+                    <ServiceBlock header="Trimming" text="As a premier local tree service, we strive to offer the highest quality pruning or trimming possible. All our pruning is done to ISA…" bText="Learn More" dest="/" />
+                    <ServiceBlock header="Surgery" text="Supplemental structural support for trees and branches. We’re experts in the cabling and bracing procedures that are the current industry…" bText="Learn More" dest="/" />
+                </TwoColRow>
+                <TwoColRow>
+                    <ServiceBlock header="Plant Health Care (PHC)" text="Our team will do everything possible to improve and maintain a tree’s health. We’ll do what is right for every tree." bText="Learn More" dest="/" />
+                </TwoColRow>
+            </ServiceList>
             <FirewoodBlock />
             <Summary bText="Get a free quote" dest="/"/>
         </Container>
