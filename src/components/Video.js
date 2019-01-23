0995c01346fe7f1d49e3441277fdde58bf28bddb
Alisha Garric <AlishaGarric@Alishas-iMac.local>
2019-01-22 16:56:53 -0800

    fixed video container height

--- src/components/Video.js
+++ src/components/Video.js
@@ -3,6 +3,6 @@ import ReactPlayer from 'react-player'
  
 export default class App extends Component {
   render () {
-      return <ReactPlayer url='https://www.dropbox.com/s/c2dswh5v23q7g24/NPTC_proof2.mp4?dl=0' playing loop height="auto" width="100%" controls/>
+      return <ReactPlayer url='https://www.dropbox.com/s/c2dswh5v23q7g24/NPTC_proof2.mp4?dl=0' loop height="54vw" width="96vw" controls/>
   }
 }
